fb402aa2d891f7c34def09e7b902b899446421d4
Bruno-Mascarenhas <brunomasck@gmail.com>
2019-06-02 12:36:58 -0300

    Update 1372.cpp

--- uri/1372.cpp
+++ uri/1372.cpp
@@ -1,19 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define int long long
-const int N = 1e5+5;
-int n,m, mat[N][N], dp[N][N];
-int32_t main(){
-    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    while(cin>>m>>n && n+m>0){
-        for(int i=0; i<m; i++)
-            for(int j=0; j<n; j++)
-                cin>>mat[i][j];
+#define endl '\n'
+const int N = 1e5+10;
+int arr[N],dpl[N],dpr[N],n,m;
 
-        for(int i=0; i<m; i++)
-            for(int j=0; j<n; j++)
-                if(i==0)
-                    dp[i] = 
-
+int main() {
+  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+  while(cin>>m>>n && n+m>0){
+    for(int i=0; i<m; i++){
+      for(int j=0; j<n; j++){
+        cin>>arr[j];
+        if(j>1)
+          dpl[j] = max(dpl[j-2]+arr[j],dpl[j-1]);
+        else if(j==0)
+          dpl[j] = arr[j];
+        else 
+          dpl[j] = max(dpl[j-1],arr[j]);
+      }
+      if(i>1)
+        dpr[i] = max(dpr[i-2]+dpl[n-1],dpr[i-1]);
+      else if(i==0)
+        dpr[i] = dpl[n-1];
+      else 
+        dpr[i] = max(dpr[i-1],dpl[n-1]);
     }
+    cout<<dpr[m-1]<<endl;
+  }
 }
